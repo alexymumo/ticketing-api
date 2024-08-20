735a07f571f8ea583ef458c271aeca1789efa359
Alex Mumo <alexymumo2000@gmail.com>
2024-08-20 17:25:46 +0300

    Add password hashing

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"events/internal/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	route := gin.New()
+	route.Use(gin.Logger())
+	routes.AuthRoutes(route)
+	route.Run("localhost:8080")
+}
--- internal/controllers/auth_controller.go
+++ internal/controllers/auth_controller.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type LoginInput struct {
+	Email    string
+	Password string
+}
+
 func CreateUser(repo repository.AuthRepository) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		var user models.User
@@ -19,12 +24,22 @@ func CreateUser(repo repository.AuthRepository) gin.HandlerFunc {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
 			return
 		}
-		ctx.JSON(http.StatusOK, gin.H{"data": "successfully created"})
+		ctx.JSON(http.StatusCreated, gin.H{"data": "user created"})
 	}
 }
 
 func LoginUser(repo repository.AuthRepository) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		var loginInput LoginInput
+		if err := ctx.ShouldBindJSON(&loginInput); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		//if err := repo.SignIn(&loginInput); err != nil {
+		//	ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to login user"})
+		//	return
+		//}
+		//ctx.JSON(http.StatusOK, gin.H{"data": "logged in"})
 
 	}
 }
